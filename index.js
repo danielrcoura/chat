76abbda0d92c9920ce7a83248067b41aaab2571c
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-12-12 07:45:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const tokenGenerator = require('./tokenGenerator');
+
+app.use(express.json());
+
+app.post('/token', (req, res) => {
+  const deviceId = req.body.deviceId
+  const identity = req.body.identity;
+
+  const token = tokenGenerator.generate(identity, deviceId)
+
+  res.send({
+    identity: identity,
+    token: token.toJwt(),
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port);
